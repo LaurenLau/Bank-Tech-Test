e37e61bd97dc53dd8262f8af18b073b358cb1191
harris_lau@hotmail.co.uk <harris_lau@hotmail.co.uk>
2019-09-25 00:37:26 +0100

    Application can now show the user a statement containing one transaction

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -30,14 +30,15 @@ describe('Bank', function() {
   });
 
   it('can show an empty bank statement', function() {
-    expect(bank.statement).toBe('date || deposits || withdrawals || balance')
+    expect(bank.showStatement()).toBe('   date   || credit || debit || balance')
   });
 
-  // it('can show one transaction', function() {
-  //   bank.deposit(100)
-  //   expect(bank.statement).toBe(
-  //     'date       || deposits || withdrawals || balance \n 23/09/2019 ||   £100   ||      £0     ||   £100'
-  //     )
-  // })
+  it('can show one transaction', function() {
+    bank.deposit(100)
+
+    expect(bank.showStatement()).toBe(
+      '   date   || credit || debit || balance\n Wed Sep 25 2019 || £0 || £100 || £100'
+      )
+  })
   
 });
